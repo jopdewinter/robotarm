5ab6dddfeaebfe7c74bec8796c13987fc869ccea
jop de winter <99039923@mydavinci.nl>
2016-10-31 09:45:00 +0100

    oefening 12 gemaakt

--- oefening 12.lua
+++ oefening 12.lua
@@ -1,32 +1,18 @@
 require 'robot_arm'
 robot_arm:random_level(1)
-robot_arm.speed = 0.9
 
-robot_arm:grab()
-robot_arm:move_right()
-robot_arm:drop()
-robot_arm:move_left()
-robot_arm:grab()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:drop()
-robot_arm:move_left()
-robot_arm:move_left()
-robot_arm:grab()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:drop()
-robot_arm:move_left()
-robot_arm:move_left()
-robot_arm:move_left()
-robot_arm:grab()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:move_right()
-robot_arm:drop()
-robot_arm:move_left()
-robot_arm:move_left()
-robot_arm:move_left()
-robot_arm:move_left()
+robot_arm.speed = 0.99
+
+for i = 1, 8 do
+  robot_arm:grab()
+  if robot_arm:scan() == nil then
+    break
+  end
+  for i = 1, i do
+    robot_arm:move_right()
+  end
+  robot_arm:drop()
+  for i = 1, i do
+    robot_arm:move_left()
+  end
+end⁠⁠
